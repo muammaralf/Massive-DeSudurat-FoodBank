129684a71d44b3d64c9c8104dd539931337557a0
muamaralf <muamar.alf@gmail.com>
2023-11-21 22:25:27 +0700

    add apa aja yg diubah

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,80 @@ const Footer = () => {
   return (
     <>
       <div className="footer-container">
-        <div className="footer-top">bla blabla</div>
+        <div className="footer-top">
+          <div className="container-text-top">
+            <div className="container-logo-footer">
+              <div className="container-logo-footer-top">
+                <img
+                  src="../src/assets/foodbank-yk-logo-orange-word.svg"
+                  className="logo-footer"
+                />
+              </div>
+              <div className="container-text1-footer-top">
+                <p>
+                  Sebagai agen perubahan yang peduli, bersama-sama kami
+                  menciptakan kisah kebersamaan yang tak terlupakan, mengubah
+                  harapan menjadi kenyataan, dan menjadi cahaya bagi mereka yang
+                  membutuhkan. Ayo bergabung dalam perjalanan kebaikan ini, dan
+                  bersama-sama, mari kita tinggalkan jejak positif yang
+                  menghadirkan kebahagiaan.
+                </p>
+              </div>
+            </div>
+            <div className="container-link-kilat">
+              <div className="container-link-kilat-top">
+                <p>Link Kilat</p>
+              </div>
+              <div className="container-link-kilat-bottom">
+                <ul>
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                    >
+                      <path
+                        d="M5 0C2.23875 0 0 2.23875 0 5C0 7.76125 2.23875 10 5 10C7.76125 10 10 7.76125 10 5C10 2.23875 7.76125 0 5 0ZM5 9.0625C2.75625 9.0625 0.9375 7.24375 0.9375 5C0.9375 2.75625 2.75625 0.9375 5 0.9375C7.24375 0.9375 9.0625 2.75625 9.0625 5C9.0625 7.24375 7.24375 9.0625 5 9.0625Z"
+                        fill="#FEB200"
+                      />
+                      <path
+                        d="M3.46436 6.90186L4.34811 7.78561L7.13373 4.99998L4.34811 2.21436L3.46436 3.09811L5.36623 4.99998L3.46436 6.90186Z"
+                        fill="#FEB200"
+                      />
+                    </svg>
+                    <p>FAQ</p>
+                  </li>
+                  <li>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                    >
+                      <path
+                        d="M5 0C2.23875 0 0 2.23875 0 5C0 7.76125 2.23875 10 5 10C7.76125 10 10 7.76125 10 5C10 2.23875 7.76125 0 5 0ZM5 9.0625C2.75625 9.0625 0.9375 7.24375 0.9375 5C0.9375 2.75625 2.75625 0.9375 5 0.9375C7.24375 0.9375 9.0625 2.75625 9.0625 5C9.0625 7.24375 7.24375 9.0625 5 9.0625Z"
+                        fill="#FEB200"
+                      />
+                      <path
+                        d="M3.46436 6.90186L4.34811 7.78561L7.13373 4.99998L4.34811 2.21436L3.46436 3.09811L5.36623 4.99998L3.46436 6.90186Z"
+                        fill="#FEB200"
+                      />
+                    </svg>
+                    <p>Support</p>
+                  </li>
+                  <li></li>
+                  <li></li>
+                  <li></li>
+                </ul>
+              </div>
+            </div>
+            <div className="container-alamat"></div>
+            <div className="container-link-sosmed"></div>
+          </div>
+        </div>
         <div className="footer-bottom">Made by De'Sudurat Team</div>
       </div>
     </>
