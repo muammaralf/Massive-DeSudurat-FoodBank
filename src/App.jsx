f1049512ebb6e225a7249ad4035d7f7718ebe044
muamaralf <muamar.alf@gmail.com>
2023-11-22 10:23:07 +0700

    footer insyaaallah selesai, ubah index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
-import Footer from "./Components/footer";
+import Footer from "./Components/Footer";
 import Dashboard from "./pages/Dashboard";
 import Informasi from "./pages/Informasi";
 import "./index.css";
